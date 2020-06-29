52e88ff642332f3707c2e64385c2a233c1d0a737
gzhanl <gzlianghan@163.com>
2020-06-30 07:26:30 +0800

    update 20200630

--- ch3/3.2.3.py
+++ ch3/3.2.3.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jun 28 23:51:07 2020
+Created on Tue Jun 30 07:16:04 2020
 
 @author: DELL
 """
 
+# =============================================================================
+# 3.3 异常处理及24小时数据挖掘实战 by 王宇韬
+# =============================================================================
 import requests
 import re
+import time
 
+#headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'}
 header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'}  # my browser
 
 
@@ -22,9 +27,7 @@ def baidu(company):
     info = re.findall(p_info, res, re.S)
     href = re.findall(p_href, res, re.S)
     title = re.findall(p_title, res, re.S)
-    
-    
-    
+
     source = []  # 先创建两个空列表来储存等会分割后的来源和日期
     date = []
     for i in range(len(info)):
@@ -35,23 +38,17 @@ def baidu(company):
         date.append(info[i].split('&nbsp;&nbsp;')[1])
         source[i] = source[i].strip()
         date[i] = date[i].strip()
+
         print(str(i + 1) + '.' + title[i] + '(' + date[i] + '-' + source[i] + ')')
         print(href[i])
 
 
-    #file1 = open('C:\\Users\DELL\Desktop\Python_Finance\MY test\ch3\数据挖掘报告.txt', 'a')  # 如果把a改成w的话，则每次生成txt的时候都会把原来的txt清空
-    file1 = open('数据挖掘报告.txt', 'a')  # 相对路径，如果把a改成w的话，则每次生成txt的时候都会把原来的txt清空
-    file1.write(company + '数据挖掘completed！' + '\n' + '\n')
-    for i in range(len(title)):
-        file1.write(str(i + 1) + '.' + title[i] + '(' + date[i] + '-' + source[i] + ')' + '\n')
-        file1.write(href[i] + '\n')  # '\n'表示换行
-    file1.write('——————————————————————————————' + '\n' + '\n')
-    file1.close()
-
-
-companys = ['华能信托', '阿里巴巴', '万科集团', '百度集团', '腾讯', '京东']
-for c in companys:
-    baidu(c)
-    print(c + '百度新闻爬取成功')
-
-print('数据获取及生成报告成功')
+while True:
+    companys = ['华能信托', '阿里巴巴', '万科集团', '百度集团', '腾讯', '京东']
+    for c in companys:
+        try:
+            baidu(c)
+            print(c + '百度新闻爬取成功')
+        except:
+            print(c + '百度新闻爬取失败')
+    time.sleep(10800)  # 每10800秒运行一次，即3小时运行一次，注意缩进
