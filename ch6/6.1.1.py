3ad06e3032303cd0f0ec8ff55c760ca3d50575bf
gzhanl <gzlianghan@163.com>
2020-08-21 23:49:24 +0800

    20200815200821—_2349

---
+++ ch6/6.1.1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 21 12:11:35 2020
+
+@author: DELL
+"""
+
+import numpy as np
+a = [1, 2, 3, 4]
+b = np.array([1, 2, 3, 4])
+print(a)
+print(b)
+print(type(a))
+print(type(b))
