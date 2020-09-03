948978956fa5b80156a4f48d1428bbb4f9f3c3a9
gzhanl <gzlianghan@163.com>
2020-09-03 08:12:45 +0800

    20200815200903_0812

---
+++ ch8/8.2.3.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+browser=webdriver.Chrome()
+
+browser.get('https://www.baidu.com')
---
+++ ch8/8.2.4.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+browser=webdriver.Chrome()
+
+browser.get('https://www.baidu.com')
+
+browser.quit()
+
